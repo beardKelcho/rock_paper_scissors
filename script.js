eb2d1bef8a450d2e23efb3963bd9a4ff8c6787fc
beardKelcho <c.kaaan@gmail.com>
2022-12-12 02:50:26 +0300

    checkwinner fonksiyonu oluşturuldu

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let computerChoices = [{choice: 'rock', value: 0},
 const playerSelection = prompt(`"Rock-Paper-Scissors" birini yazınız`,"").toLowerCase();
 const computerSelection = getComputerChoice();    
 
+
     function getComputerChoice(){ 
     let result = computerChoices[Math.floor(Math.random() * 
         computerChoices.length)];
@@ -14,8 +15,34 @@ const computerSelection = getComputerChoice();
 
 function playRound( playerSelection, computerSelection ){
 
-    
+    let result = checkWinner(playerSelection,computerSelection);
+    console.log(result);
+    return result;
     
 }
 
-console.log(playRound(playerSelection,computerSelection));
+// console.log(playRound(playerSelection,computerSelection));
+
+function game (playerSelection,computerSelection){
+    // for (let i = 0; i < 5; i++){
+    //     if (playerSelection=="Rock")
+    // }
+    console.log(playRound(playerSelection,computerSelection));
+
+}
+function checkWinner (choice1,choice2){
+   if (
+        (choice1 == "rock" && choice2 == "scissors") ||
+        (choice1 == "paper" && choice2 == "rock") ||
+        (choice1 == "scissors" && choice2 == "paper")
+      ) {
+        return "Player";
+    } else if (choice1 == choice2) {
+        return "Tie";
+    } else {
+        return "Computer";
+    }
+    
+}
+
+game();
